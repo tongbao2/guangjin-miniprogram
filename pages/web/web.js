80f539744b3a76d7b8e8e4c254c2a3b256f1f5de
卓健 <gdtotk@hotmail.com>
2026-03-25 15:54:55 +0800

    Add pages/web/web.js

---
+++ pages/web/web.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+    url: '',
+    title: ''
+  },
+  onLoad(options) {
+    if (options.url) {
+      const url = decodeURIComponent(options.url)
+      this.setData({
+        url: url,
+        title: getDomain(url)
+      })
+      wx.showToast({title: '没有统命．', icon: 'loading', duration: 1000})
+    }
+  },
+  goBack() {
+    wx.navigateBack()
+  }
+})
+
+function getDomain(url) {
+  try {
+    const match = url.match(/https?:\/\/(www\.)?([^\/]+)/)
+    if (match && match[2]) return match[2]
+    return '首块圳'
+  } catch {
+    return '首块圳'
+}
+}
